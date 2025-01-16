1c441e674ed052896a4bb3256004d050fd9a79d8
Admin <ubaldha434@rku.ac.in>
2025-01-16 09:23:58 +0530

    add post and put method

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ var students = [
     roll_no: 62,
   },
 ];
-
+//To fetch all students
 app.get("/", (req, res) => res.json(students));
+//to add new students
 app.post("/", (req, res) => {
-    const {enrollment_no,name,branch,sem,div,roll_no} = req.body;
+    const { enrollment_no,name,branch,sem,div,roll_no } = req.body;
     students = [...students,{ enrollment_no,name,branch,sem,div,roll_no }];
+    res.json(students);
+});
+
+//to update a student
+
+app.put("/:enrollment_no", (req, res) => {
+    const enrollment_no  = req.params.enrollment_no;
+    const { name,branch,sem,div,roll_no } = req.body;
+    students = students.map((student) => student.enrollment_no == enrollment_no ? { enrollment_no,name,branch,sem,div,roll_no } : student );
     res.json(students);
 });
 app.listen(port, () =>
